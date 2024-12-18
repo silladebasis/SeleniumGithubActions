ed66eac58cd8060b4b5957652dba2351fd07b8fb
Debasis <silladebasis2023@gmail.com>
2024-12-18 14:21:30 -0500

    updated gitignore file

--- src/main/java/driver/ChromeDriverManager.java
+++ src/main/java/driver/ChromeDriverManager.java
@@ -8,6 +8,9 @@ public class ChromeDriverManager implements BrowserDriver{
     @Override
     public WebDriver createDriver() {
         ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");  // Run in headless mode
+        chromeOptions.addArguments("--disable-gpu");  // Disable GPU hardware acceleration
+        chromeOptions.addArguments("--no-sandbox");
         return new ChromeDriver(chromeOptions);
     }
 }
